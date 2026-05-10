d3da659fa8077e6c847fe6fed5cff392d3bafa23
Osvaldo Cisneros <cifmosvaldo@gmail.com>
2026-05-10 09:00:49 -0600

    modificacion multimedia

--- frontend/src/components/Gallery.jsx
+++ frontend/src/components/Gallery.jsx
@@ -7,6 +7,30 @@ export default function Gallery() {
                 <img className="img-gallery" src="/assets/fotos/img2.png" alt="Proyecto 1" />
                 <img className="img-gallery" src="/assets/fotos/img3.png" alt="Proyecto 1" />
 
+
+
+                {/* Video 1 */}
+                <video
+                    className="video-gallery"
+                    controls
+                    width="400"
+                >
+                    <source src="/assets/videos/Estando_video.mp4" type="video/mp4" />
+                    Tu navegador no soporta videos.
+                </video>
+
+                {/* Video 2 */}
+                <video
+                    className="video-gallery"
+                    controls
+                    width="400"
+                >
+                    <source src="/assets/videos/Sierena_video.mp4" type="video/mp4" />
+                    Tu navegador no soporta videos.
+                </video>
+
+
+
             </div>
         </section>
     );
